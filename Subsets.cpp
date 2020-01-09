9977d0f0a6c483badec493c36982d6c1e387b0e9
Shreya Prasad <shreyapd06@gmail.com>
2020-01-09 17:42:25 +0530

    Create Subsets.cpp

---
+++ Subsets.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> subs = {{}};
+        for (int num : nums) {
+            int n = subs.size();
+            for (int i = 0; i < n; i++) {
+                subs.push_back(subs[i]); 
+                subs.back().push_back(num);
+            }
+        }
+        return subs;
+    }
+};
+
+//Time Complexity: O(n^2)
+//LeetCode: https://leetcode.com/problems/subsets/
