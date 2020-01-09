db8784c8c5e36ef4680849d29c161e0debc6a9f9
Ekansh Gupta <33174166+eKuG@users.noreply.github.com>
2020-01-10 02:55:19 +0530

    Update HouseRobber.cpp

--- HouseRobber.cpp
+++ HouseRobber.cpp
@@ -15,3 +15,29 @@ public:
 
 //Time Complexity: O(n)
 //LeetCode: https://leetcode.com/problems/house-robber/
+
+
+//_______________________________________________//
+//eKuG
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if(nums.size()==0){
+            return 0;
+        }
+        if(nums.size()==1){
+            return nums[0];
+        }
+        if(nums.size()==2){
+            return max(nums[0],nums[1]);
+        }
+        vector<int> cost(nums.size(),0);
+            cost[0]=nums[0];
+            cost[1]= max(nums[0], nums[1]);
+        for(int i = 2; i < nums.size(); i++){
+            cost[i]=max(cost[i-1],(cost[i-2]+nums[i]));
+        }
+        return cost[cost.size()-1];
+    }
+};
